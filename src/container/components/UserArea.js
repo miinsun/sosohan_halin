0101a26e0b1304b38924a03b83325c8ab81b31c5
hyeyeon <merry5596@naver.com>
2021-09-25 03:46:58 +0900

    빨간줄 수정

--- src/container/components/UserArea.js
+++ src/container/components/UserArea.js
@@ -21,34 +21,35 @@ const UserArea = () => {
   };
 
   return (
-      <div className="UserArea container">
-       <div className="row">
+    <div className="UserArea container">
+      <div className="row">
         {!sessionStorage.getItem("sessionId") && (
-            <div className="GuestArea">
-                <div className="col">
-                    <Link to="/login" className="btn btn-secondary">로그인</Link>
-                </div>
+          <div className="GuestArea">
+            <div className="col">
+              <Link to="/login" className="btn btn-secondary">로그인</Link>
             </div>
+          </div>
         )}
-       {sessionStorage.getItem("sessionId") && (
-            <div className="LoginArea">
-              <div className="col">
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <Link to="/editaccount">{sessionId}님</Link>
-              </div>
-              <div className="col">
-                <SelectShop />
-              </div>
-              <div className="col">
-                <LinkAlarm />
-              </div>
-              <div className="col">
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <Link onClick={logout} className="btn btn-secondary">로그아웃</Link>
-              </div>
-            </div>)
-        </div>
+        {sessionStorage.getItem("sessionId") && (
+          <div className="LoginArea">
+            <div className="col">
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <Link to="/editaccount">{sessionId}님</Link>
+            </div>
+            <div className="col">
+              <SelectShop />
+            </div>
+            <div className="col">
+              <LinkAlarm />
+            </div>
+            <div className="col">
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <Link onClick={logout} className="btn btn-secondary">로그아웃</Link>
+            </div>
+          </div>
+        )}
       </div>
+    </div>
   );
 };
 export default UserArea;
