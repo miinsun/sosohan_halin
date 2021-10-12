e70fb60f4dfc17479f2c97ad0cebbab6bc09fa61
hyeyeon <merry5596@naver.com>
2021-10-12 17:39:42 +0900

    [UPDATE] app 쿠폰함 api 받아와서 리스트 출력

--- shh_app/src/remote/api/consumerCouponApi.js
+++ shh_app/src/remote/api/consumerCouponApi.js
@@ -1,23 +1,8 @@
 import client from "../client";
 import urls from "../urls";
 
-export const post = (consumerAccountId, couponId) => {
-    console.log(`${urls.consumerCoupon}/${consumerAccountId}/${couponId}`);
-    client.post(`${urls.consumerCoupon}/${consumerAccountId}/${couponId}`);
-};
-
-// export const post = (consumerAccountId, couponId) => {
-//     client.post(`${urls.consumerCoupon}/${consumerAccountId}/${couponId}`)
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
-// };
-
-
-export const getAll = (consumerAccountId, params) => client.get(`${urls.consumerCoupon}/${consumerAccountId}?${params}`);
+export const post = (receipt, couponId) => client.post(`${urls.consumerCoupon}/${couponId}`, receipt);
+export const getAll = (consumerUserId, params) => client.get(`${urls.consumerCoupon}/${consumerUserId}?${params}`);
 export const get = (consumerAccountId, consumerCouponId) => client.get(`${urls.consumerCoupon}/${consumerAccountId}/${consumerCouponId}`);
 // put == use
 export const put = (consumerCouponId) => client.put(`${urls.consumerCoupon}/${consumerCouponId}`);
